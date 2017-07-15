a835672ca429df5e3a73dc66dd1495ff3a120a47
Sumit Sati <sumit.sati@stud.tu-darmstadt.de>
2017-07-15 18:33:56 +0200

    few more

---
+++ Warmup-1/diff21.java
@@ -0,0 +1,18 @@
+//Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
+//
+//diff21(19) → 2
+//diff21(10) → 11
+//diff21(21) → 0
+
+public int diff21(int n) {
+  if (n>21){
+    return (2*(n-21));
+  }
+  else if (n <0){
+    int y = (n-21);
+    return (y- (2*y));
+  }
+  else
+    return (21 -n);
+}
+
